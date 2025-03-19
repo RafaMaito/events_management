715fda5d3d66c7289d68416505c1aec539297e40
Rafael Maito <rafaelmaito7@gmail.com>
2025-03-19 01:31:35 -0300

    EM-Drupal-04: Create a custom column to add the weather city

--- web/modules/custom/events/src/Controller/EventsController.php
+++ web/modules/custom/events/src/Controller/EventsController.php
@@ -3,11 +3,25 @@
 namespace Drupal\events\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\events\Service\WeatherService;
 
 class EventsController extends ControllerBase {
+
+  protected $weatherService;
+
+  // We need to inject the WeatherService service into the controller.
+  public function __construct(WeatherService $weatherService) {
+    $this->weatherService = $weatherService;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('events.weather_service')
+    );
+  }
+
   public function list() {
-    return [
-      '#markup' => $this->t('Events list'),
-    ];
+
   }
 }
